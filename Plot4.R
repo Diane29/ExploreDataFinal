1c1b8dca1dac56ddaadc56ecd9e8034e4beaeae9
Diane29 <diandradiane@aol.com>
2016-04-10 22:58:27 -0400

    Create Plot4.R

---
+++ Plot4.R
@@ -0,0 +1,20 @@
+#Read in the data
+library(ggplot2)
+setwd("~/Explore")
+NEI <- readRDS("~/Explore/summarySCC_PM25.rds")
+SCC <- readRDS("~/Explore/Source_Classification_Code.rds")
+
+# Select data from Coal Combustion Sources
+SCC_coal = SCC[grepl("coal", SCC$Short.Name, ignore.case=TRUE),] 
+NEI_SCC_coal <- merge(x=NEI, y=SCC_coal, by='SCC') 
+
+#Get the Total Country-wide Emissions by Year and Assign Column names
+NEI_SCC_coal <- aggregate(NEI_SCC_coal$Emissions,list(NEI_SCC_coal$year),FUN=sum)
+colnames(NEI_SCC_coal) <- c("Year","Tot_Emissions")
+
+qplot(Year,Tot_Emissions,data=NEI_SCC_coal, geom = c("point", "line"),
+      ylab="Total PM2.5 Emissions (tons)",
+      main = "Coal-Combustion Sources PM2.5 Emissions from 1999-2008")
+
+dev.copy(png, file="plot4.png", height=480, width=480) 
+dev.off()
