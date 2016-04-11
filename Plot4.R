959547c6012c4a5d0d2e281d84ecd113daeb68b7
Diane29 <diandradiane@aol.com>
2016-04-10 23:04:26 -0400

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -4,7 +4,7 @@ setwd("~/Explore")
 NEI <- readRDS("~/Explore/summarySCC_PM25.rds")
 SCC <- readRDS("~/Explore/Source_Classification_Code.rds")
 
-# Select data from Coal Combustion Sources
+#Select data from Coal Combustion-related sources
 SCC_coal = SCC[grepl("coal", SCC$Short.Name, ignore.case=TRUE),] 
 NEI_SCC_coal <- merge(x=NEI, y=SCC_coal, by='SCC') 
 
@@ -14,7 +14,7 @@ colnames(NEI_SCC_coal) <- c("Year","Tot_Emissions")
 
 qplot(Year,Tot_Emissions,data=NEI_SCC_coal, geom = c("point", "line"),
       ylab="Total PM2.5 Emissions (tons)",
-      main = "Coal-Combustion Sources PM2.5 Emissions from 1999-2008")
+      main = "Coal Combustion sources PM2.5 Emissions from 1999-2008")
 
 dev.copy(png, file="plot4.png", height=480, width=480) 
 dev.off()
